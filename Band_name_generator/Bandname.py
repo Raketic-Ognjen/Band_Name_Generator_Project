3ac7caa00da565ec281bede95cc4e44b852b2c8a
OgnjenR <ognjenraketic23@outlook.com>
2022-09-04 00:25:54 +0200

    Initial commit

---
+++ Band_name_generator/Bandname.py
@@ -0,0 +1,9 @@
+#Create a greeting for your program
+print("This is Band Name Generator!")
+#Ask the user for the city that they grow up in.
+city = input("Where are you from?\n")
+#Ask the user for the name of a car
+car = input("What is the brand of your car?\n")
+#Combine name of their city and pet and show them their band name
+print("Possible name for your band is:" + city + " " + car)
+#make sure the input cursor shows on a new line
